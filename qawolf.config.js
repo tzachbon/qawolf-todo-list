da5ce1893fb810e9605735d36d3f71f44b14f7e8
tzachbon <tzachb@wix.com>
2020-06-02 16:18:31 +0300

    fix: rootDir tyop

--- qawolf.config.js
+++ qawolf.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: 'node_modules/qawolf/ts-jest.config.json',
-  rootDir: '__test__',
+  rootDir: '__tests__',
   testTimeout: 60000,
   useTypeScript: true,
   attribute: 'data-hook',
