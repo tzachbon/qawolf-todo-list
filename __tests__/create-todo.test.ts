d84d80284d01965ad4444d0ad6f7a1273df82523
tzachbon <tzachb@wix.com>
2020-06-03 09:36:49 +0300

    First real test

---
+++ __tests__/create-todo.test.ts
@@ -0,0 +1,25 @@
+import { Browser, Page } from "playwright";
+import qawolf from "qawolf";
+
+let browser: Browser;
+let page: Page;
+
+beforeAll(async () => {
+  browser = await qawolf.launch();
+  const context = await browser.newContext();
+  await qawolf.register(context);
+  page = await context.newPage();
+});
+
+afterAll(async () => {
+  await qawolf.stopVideos();
+  await browser.close();
+});
+
+test("create-todo", async () => {
+  await page.goto("http://localhost:3000/");
+  await page.click('[data-hook="wsr-input"]');
+  await page.press('[data-hook="wsr-input"]', "CapsLock");
+  await page.type('[data-hook="wsr-input"]', "Some todo");
+  await page.press('[data-hook="wsr-input"]', "Enter");
+});
